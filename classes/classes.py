ea9c794d36673c4b0fb0c3c77a4b78441c4fc34b
LFM <146396539+lfm-dev@users.noreply.github.com>
2023-12-03 16:03:25 -0300

    classes.py

---
+++ classes/classes.py
@@ -0,0 +1,22 @@
+class Entry:
+    def __init__(self, id_, name, author):
+        self.id_ = id_
+        self.name = name
+        self.author = author
+        self.quotes = []
+        self.n_quotes = 0
+        self.tags = []
+        self.reading_date = self.get_reading_date()
+
+    def add_quote(self, quote):
+        self.quotes.append(quote)
+        self.n_quotes += 1
+
+    def add_tags(self, line):
+        self.tags = line.strip()[1:-1].split(',')
+
+    def get_reading_date(self):
+        return '20' + self.id_[1:self.id_.find('-')] # this will work until the year 2100
+
+    def __repr__(self):
+        return f'{self.id_} {self.name} {self. author} {self.n_quotes}'
