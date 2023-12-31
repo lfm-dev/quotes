b61cc44e7e4bf06efe089069bed2b9bed927f1ff
LFM <146396539+lfm-dev@users.noreply.github.com>
2023-12-31 16:14:04 -0300

    strict book id query

--- classes/DB.py
+++ classes/DB.py
@@ -53,7 +53,7 @@ class DB:
     def get_book_quotes(self, book_id):
         con = self.get_conection()
         cursor = self.get_cursor(con)
-        cursor.execute(f"SELECT quote FROM quotes WHERE book_id LIKE '%{book_id}%'")
+        cursor.execute(f"SELECT quote FROM quotes WHERE book_id LIKE '{book_id}'")
         quotes = cursor.fetchall()
         self.commit_and_close(con)
         quotes = [quote[0] for quote in quotes] # fetchall returns list of tuples
