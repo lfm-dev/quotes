2af54a1767addf1f2447122710e816b8c93b3c6a
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-18 10:25:17 -0300

    refactoring

--- src/quotes.py
+++ src/quotes.py
@@ -9,18 +9,23 @@ QUOTES_PATH = '/path/to/your/quotes/folder'
 
 def main():
     os.chdir(QUOTES_PATH)
+
     books_table = BooksTable()
     quotes_table = QuotesTable()
 
     args, parser = get_args()
-    if args.makedb: # make (or update) sqlite3 db
+
+    if args.makedb:
         make_update_db(books_table, quotes_table)
+
     elif args.books:
         books = books_table.get_books(query=args.books) # query: book_name or author
         print_books_table(books)
+
     elif args.quotes:
         quotes = quotes_table.get_quotes_by_bookid(query=args.quotes) # query: book_id
         print_quotes_markdown(quotes)
+
     else:
         parser.print_help()
 
