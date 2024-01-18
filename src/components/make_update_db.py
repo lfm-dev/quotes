63202b10cf05155b2234bdd13c8b77f6ec7ca103
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-18 10:59:09 -0300

    fix: error on books without quotes

--- src/components/make_update_db.py
+++ src/components/make_update_db.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from classes.Quote import Quote
 from classes.Book import Book
 
@@ -11,25 +12,30 @@ def read_md_file(md_file_name):
                 continue
             if line.startswith('#'):
                 book_id, book_name, author = get_book_data(line)
-                quotes[book_id] = []
                 book = Book(book_id, book_name, author)
                 books[book.book_id] = book
+                quotes[book_id] = []
             elif line.startswith('*'):
                 book.n_quotes += 1
                 quotes[book_id].append('')
                 quotes[book_id][-1] += line.lstrip('* ')
             else:
                 quotes[book_id][-1] += line
-    quotes = [Quote(f'{n}-{book_id}', book_id, books[book_id].book_name, quote) for book_id in quotes for n, quote in enumerate(quotes[book_id])]
-    books = [books[book_id] for book_id in books]
+    quotes = [Quote(f'{n}-{book_id}', book_id, books[book_id].book_name, quote) for book_id in quotes for n, quote in enumerate(quotes[book_id]) if len(quotes[book_id]) > 0]
+    books = [books[book_id] for book_id in books if books[book_id].n_quotes > 0]
     return books, quotes
 
 def get_book_data(line):
-    return line[1:].strip().split('/')
+    book_data = line[1:].strip().split('/')
+    if len(book_data) != 3:
+        print(f'Wrong format in book header -> {line.strip()}\nCorrect format and run -makedb again')
+        sys.exit(1)
+    return book_data
 
 def make_update_db(books_table, quotes_table):
     quotes_files = sorted([xfile for xfile in os.listdir(os.curdir) if xfile.endswith('.md')])
     for md_file_name in quotes_files:
         books, quotes = read_md_file(md_file_name)
-        books_table.add_books(books)
-        quotes_table.add_quotes(quotes)
+        if books and quotes:
+            books_table.add_books(books)
+            quotes_table.add_quotes(quotes)
