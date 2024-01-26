b1a8a2a4f1f04f4c09ee551f8153edecc42b6c38
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-01-26 17:30:45 -0300

    TODO

--- src/quotes.py
+++ src/quotes.py
@@ -5,6 +5,7 @@ from components.make_update_db import make_update_db
 from components.get_args import get_args
 
 #TODO installation script
+#TODO add book tags support
 QUOTES_PATH = '/path/to/your/quotes/folder'
 
 def main():
