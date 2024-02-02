a3a0260fb0f40815cc52f5637ed4b03fe95e46d4
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-02-02 16:33:34 -0300

    added get_year_read() method in Book class

--- src/classes/Book.py
+++ src/classes/Book.py
@@ -3,11 +3,14 @@ class Book:
         self.book_id = book_id
         self.book_name = book_name
         self.author = author
-        self.year_read = int(book_id.split('-')[0])
+        self.year_read = self.get_year_read()
         self.n_quotes = n_quotes
 
     def get_data(self):
         return (self.book_id, self.book_name, self.author, self.n_quotes)
+
+    def get_year_read(self):
+        return int(self.book_id.split('-')[0])
 
     def __repr__(self):
         return f'{self.book_id} {self.book_name} {self. author} {self.n_quotes}'
