8cf18b9f1a06825dc3aa0f07c3ade4b7e0f6a8de
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-03-19 16:13:05 -0300

    refactoring get_authors()

--- src/components/get_authors.py
+++ src/components/get_authors.py
@@ -3,13 +3,11 @@ from classes.Author import Author
 def get_authors(books):
     authors = {}
     for book in books.values():
-        if book.author in authors:
-            authors[book.author].books.append(book)
-        else:
-            author = Author(book.author)
-            author.books.append(book)
-            author.tags += list(set(author.tags + book.tags))
-            authors[book.author] = author
+        if book.author not in authors:
+            authors[book.author] = Author(book.author)
+
+        authors[book.author].books.append(book)
+        authors[book.author].tags = list(set(authors[book.author].tags + book.tags))
 
     authors = list(authors.values())
     authors.sort(key = lambda x: x.name)
