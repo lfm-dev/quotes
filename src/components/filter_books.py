a71282d878c0dc2d8dc30ae47cbd52c4a9ad307d
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-03-19 15:58:03 -0300

    refactoring filter_books()

--- src/components/filter_books.py
+++ src/components/filter_books.py
@@ -4,6 +4,8 @@ def filter_books(books, query):
     '''
     filtered_books = []
     for book in books.values():
-        if query in book.book_name.casefold() or query in book.author.casefold() or query in book.tags or not query:
+        if not query:
+            filtered_books = list(books.values())
+        elif query in book.book_name.casefold() or query in book.author.casefold() or query in book.tags:
             filtered_books.append(book)
     return filtered_books
