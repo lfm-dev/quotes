3be0c11a551426af2f7e3fdb0ffbebf91a891dc0
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-03-23 16:44:40 -0300

    filter book by string, not substring

--- src/components/filter_books.py
+++ src/components/filter_books.py
@@ -1,9 +1,9 @@
 def hit(book, query):
     if query == book.book_id:
         return True
-    if query in book.book_name.casefold():
+    if query in book.book_name.casefold().split():
         return True
-    if query in book.author.casefold():
+    if query in book.author.casefold().split():
         return True
     if query in book.tags:
         return True
