d384c9e27a82766ecea8a3d6e489a129f4a122f7
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-03-23 13:36:28 -0300

    updated installation bash script

--- install.sh
+++ install.sh
@@ -8,19 +8,22 @@ if [ -d "/usr/local/bin/quotes_files" ]; then
     sudo rm -r "/usr/local/bin/quotes_files"
 fi
 
-while : ; do
-    echo -n "path to your quotes: "
-    read pathToQuotes
-    if [ -d "$pathToQuotes" ]; then
-        break
-    else
-        echo "$pathToQuotes doesnt exists, try again."
-    fi
-done
+if [ ! -d "$HOME/.config/quotes" ]; then
+    mkdir "$HOME/.config/quotes"
+    while : ; do
+        echo -n "path to your quotes: "
+        read pathToQuotes
+        if [ -d "$pathToQuotes" ]; then
+            echo "quotes_path,$pathToQuotes" > $HOME/.config/quotes/settings.csv
+            break
+        else
+            echo "$pathToQuotes doesnt exists, try again."
+        fi
+    done
+fi
 
 wget http://github.com/lfm-dev/quotes/archive/master.zip -O quotes.zip
 unzip quotes.zip
-sed -i -e "s#QUOTES_PATH = '/path/to/your/quotes/folder'#QUOTES_PATH = '$pathToQuotes'#" quotes-main/src/quotes.py # add path to script
 sudo cp -r quotes-main/src /usr/local/bin/quotes_files
 rm -r quotes-main quotes.zip
 cd /usr/local/bin
