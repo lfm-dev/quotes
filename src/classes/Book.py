0f60cde194cb25863029241ed21b00337972da24
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-04-02 15:17:19 -0300

    added favorite books support

--- src/classes/Book.py
+++ src/classes/Book.py
@@ -1,9 +1,10 @@
 class Book:
-    def __init__(self, book_id, book_name, author, reading_year):
+    def __init__(self, book_id, book_name, author, reading_year, is_favorite):
         self.book_id = book_id
         self.book_name = book_name
         self.author = author
         self.reading_year = reading_year
+        self.is_favorite = is_favorite
         self.quotes = []
         self.tags = []
 
--- src/components/get_books.py
+++ src/components/get_books.py
@@ -13,8 +13,8 @@ def read_md_file(md_file_name):
             if line.startswith('#'):
                 book_number += 1
                 book_id = f'{year[-2:]}-{book_number}'
-                book_name, author = get_book_name_author(line, md_file_name)
-                books[book_id] = Book(book_id, book_name, author, year)
+                book_name, author, is_favorite = get_book_name_author(line, md_file_name)
+                books[book_id] = Book(book_id, book_name, author, year, is_favorite)
             elif line.startswith('[') and line.strip().endswith(']'):
                 books[book_id].tags = line.lower().strip('[]\n').split(',')
             elif line.startswith('*'): # new quote starts
@@ -24,14 +24,15 @@ def read_md_file(md_file_name):
     return books
 
 def get_book_name_author(line, file_name):
+    is_favorite = line.strip().endswith('*')
     try:
-        book_name, author = line.strip('#\n').split('/')
+        book_name, author = line.strip('*#\n').split('/')
     except ValueError:
         print(f'Wrong format in book header -> {line.strip()} in {file_name}')
         print('The format should be book_name/author')
         print('Correct format and run -makedb again')
         sys.exit(1)
-    return book_name, author
+    return book_name, author, is_favorite
 
 def get_books():
     quotes_files = sorted([xfile for xfile in os.listdir(os.curdir) if xfile.endswith('.md')])
