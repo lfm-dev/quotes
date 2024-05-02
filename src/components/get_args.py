70db5fa7840fb795d5fbc4b3ee342cb15e91adc8
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-05-02 19:03:41 -0300

    updated help

--- src/components/get_args.py
+++ src/components/get_args.py
@@ -2,8 +2,8 @@ import argparse
 
 def get_args():
     parser = argparse.ArgumentParser(prog='quotes', description='Manage your book quotes')
-    parser.add_argument('-b', '-books', metavar='', dest='show_books', nargs='?', const=' ', help='search books by name, author, tag, reading year or favorites (favs) (-b shows all books)')
-    parser.add_argument('-q', '-quotes', metavar='', dest='show_quotes', help='show all quotes of a book by book ID')
+    parser.add_argument('-b', '-books', metavar='', dest='show_books', nargs='?', const=' ', help='search books by book id, book name, author, tag or reading year (empty to show all books / favs to show favorites)')
+    parser.add_argument('-q', '-quotes', metavar='', dest='show_quotes', help='search quotes by a query word')
     parser.add_argument('-a', '-authors', action='store_true', dest='show_authors', help='show authors table')
     parser.add_argument('-t', '-tags', action='store_true', dest='show_tags', help='show tags table')
     args = parser.parse_args()
